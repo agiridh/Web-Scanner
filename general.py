85488c6e5a913a3311b4cdf271497de75ce3628d
Aditya Giridhar <agiridh@purdue.edu>
2015-12-31 14:13:58 +0530

    wrote functions to create a directory and to write to a file

---
+++ general.py
@@ -0,0 +1,12 @@
+import os
+
+# create a directory if it does not exist already
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+# writing data to a file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close
