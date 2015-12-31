0384337b117daee6ce1126e65dd3c420f16cf844
Aditya Giridhar <agiridh@purdue.edu>
2015-12-31 14:47:28 +0530

    added function to get top level domain

---
+++ domain_name.py
@@ -0,0 +1,7 @@
+from tld import get_tld
+
+# get the top level domain.
+# Ex: print get_domain_name("http://www.google.com") prints google.com
+def get_domain_name(url):
+    domain_name = get_tld(url)
+    return domain_name
