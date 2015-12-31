f0c8979b34d3919042b5e21dbba991f120e81ac7
Aditya Giridhar <agiridh@purdue.edu>
2015-12-31 16:29:40 +0530

    added file to do an nmap scan

---
+++ nmap.py
@@ -0,0 +1,12 @@
+import os
+
+# nmap scans a server to see what processes are running and what ports are open
+# print get_nmap('-F', '216.58.197.46')
+# google nmap for more nmap flags. -F is does a fast scan
+def get_nmap(options, ip):
+    command = 'nmap ' + options + ' ' + ip
+    process = os.popen(command) # starts another process. (like typing a new command on terminal)
+    results = str(process.read())
+    return results
+
+print get_nmap('', '216.58.197.46')
